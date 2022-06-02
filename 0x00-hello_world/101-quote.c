f9ecc35baa339dfcca774cd8c870bee57c0409bd
tbir8256 <tbir8256@gmail.com>
2022-06-02 15:32:13 -0700

    101.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,8 @@
  * Description: prints Dors's quote
  * Return: Always 1 (Success)
  */
-
 int main(void)
-
 {
-
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
