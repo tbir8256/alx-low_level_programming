e5c089f2e1bd5b53ff531aeb233c79fc452f51fe
tbir8256 <tbir8256@gmail.com>
2022-06-02 14:47:44 -0700

    101.quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ *  101-quote.c
+ *  program that prints piece of art is useful\" - Dora Korpar, 2015-10-19
+  */
+
+#include <stdio.h>
+
+/**
+ * main - My entry point for my program
+ * Return: returns 0
+ */
+
+int main(void)
+
+{
+
+		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+			return (0);
+
+}
