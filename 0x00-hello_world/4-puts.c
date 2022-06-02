a801fcd176c356386a5a42d42118e45353f7483b
tbir8256 <tbir8256@gmail.com>
2022-06-02 13:55:17 -0700

    4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-
-/**
- *
- * Description: main - Prints out a string to stdout.
- *
- * Return: 0 if success.
- *
- */
-
 int main(void)
 
 {
 
-	Puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 
 	return (0);
 
