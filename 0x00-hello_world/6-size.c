54a7dcfd043964ea31453797261d9bf3bffdf514
tbir8256 <tbir8256@gmail.com>
2022-06-02 14:38:07 -0700

    6.-Size & territory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,26 @@
 int main(void)
 {
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+ar c;
 
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+int i;
 
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+long l;
 
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+long long ll;
 
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+float f;
 
-	return (0);
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return (0);
 
 }
