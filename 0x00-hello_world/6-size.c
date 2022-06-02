bd1a74629ebe7f60ed647755a48a4f442df58ae7
tbir8256 <tbir8256@gmail.com>
2022-06-02 14:29:26 -0700

    6-Size & territory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
  * main - this program display the size of the different data types
  * return: 0 for main function
  */
-
-int main(void)
+void main(void)
 {
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
