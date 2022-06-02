0d5992289116c385f29c0c1ad3d0404a11e29332
tbir8256 <tbir8256@gmail.com>
2022-06-02 13:28:45 -0700

    4.Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/* a C program that prints exactly "Programming is like building a multilingual puzzle  * */ 
+
+
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
