0b35c340e097eba5236c40e6ccb340b4ce506551
tbir8256 <tbir8256@gmail.com>
2022-06-03 13:42:46 -0700

    4-print Alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/*
  * main-print letters
  *
  * Description: print the lettere in alphabet
@@ -8,13 +8,13 @@
  * Return: returns 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 int i = 97;
 
 while (i < 123)
 {
-if (i!=101 && i !=113)
+if (i != 101 && i != 113)
 {
 putchar(i);
 }
