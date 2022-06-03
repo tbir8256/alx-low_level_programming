043c9f03e58541c9e34273a7e53acf5395fd10c9
tbir8256 <tbir8256@gmail.com>
2022-06-03 13:58:37 -0700

    5.Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+int i;
+i = 48;
+while(i < 58)
+{
+	putchar(i);
+	i++;
+}
+return (0);
+
+}
