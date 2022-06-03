0409a5a0be04ed565f7836f3022b648a275d2fb2
tbir8256 <tbir8256@gmail.com>
2022-06-03 14:07:04 -0700

    6.Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main-print single digit numbers
+ *
+ * Description: print single 
+ *
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+int i;
+i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar (10);
+return (0);
+}
+
