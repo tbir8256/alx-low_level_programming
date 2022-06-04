e87ad03423d5b58c0ca1798e97c6cfa60bf72dd0
tbir8256 <tbir8256@gmail.com>
2022-06-03 19:03:59 -0700

    101-Print_combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - print three digits
+ * main-print three digits
  *
  * Description: prints three digits
  *
- * Retrun: returns 0 (sucessful)
+ * Retrun: returns 0 (sucess)
  */
 
 int main(void)
 {
 int i, j, k;
+
+i = 48;
+j = 48;
+k = 48;
 
 while (i < 58)
 {
@@ -23,7 +27,7 @@ while (k < 58)
 putchar(i);
 putchar(j);
 putchar(k);
-if (i < 55 || j < 56 || k <57)
+if (i < 55 || j < 56 || k < 57)
 {
 putchar(44);
 putchar(32);
