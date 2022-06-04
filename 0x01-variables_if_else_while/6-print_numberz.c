4243239e6e0fc34f0bee1137d1a1f1c04272c226
tbir8256 <tbir8256@gmail.com>
2022-06-03 20:41:13 -0700

    6-Print Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - print single digit numbers 
+ * main - print single digit numbers
+ * 
  * Description: print single
+ *
  * Return: return 0 (success)
  */
 
 int main(void)
 {
 int i;
+
 i = 48;
 while (i < 58)
 {
