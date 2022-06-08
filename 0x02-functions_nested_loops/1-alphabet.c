70987b131b252e783c68a0b9f15330d86192ce9c
tbir8256 <tbir8256@gmail.com>
2022-06-07 19:11:12 -0700

    1. print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  * Return: returns 0 (scucess)
  */
 
-void print(void)
+void print_alphabet(void)
 {
 int i;
-for (i = 'a'; i <= 'z'; j++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
-_putchar('\n')
+_putchar('\n');
 }
