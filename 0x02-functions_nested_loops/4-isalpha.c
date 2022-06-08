f7f14052f695454950e3a48ff65c73ae5846cb73
tbir8256 <tbir8256@gmail.com>
2022-06-07 20:13:49 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for letters
+ * @c: a charater to be checked
+ * Return: returns 0 or 1
+ */
+
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'));
+}
