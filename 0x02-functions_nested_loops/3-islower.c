177a0f3162014f313fe2af1c8303af3ac08b432e
tbir8256 <tbir8256@gmail.com>
2022-06-07 20:02:50 -0700

    3-Lowerc

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /*
- * _islower -> checksif the character is lowwer
+ *_islower -> checks if the character is lowwer
  * Return: returns 0
  */
+
 int _islower(int c)
 {
 return (c >= 'a' && c <= 'z');
