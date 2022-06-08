c47d35148d130614e91acf9c944117a269a93027
tbir8256 <tbir8256@gmail.com>
2022-06-07 19:06:32 -0700

    1. print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- Prints alphate in lowercase
+ * Return: returns 0 (scucess)
+ */
+
+void print(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; j++)
+{
+_putchar(i);
+}
+_putchar('\n')
+}
