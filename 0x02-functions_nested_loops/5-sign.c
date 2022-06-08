e02606484d38afd396ee57c248685e2647e2af05
tbir8256 <tbir8256@gmail.com>
2022-06-07 20:31:32 -0700

    5-.Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
 
 int print_sign(int n)
 {
-if ( n > 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
@@ -15,10 +15,11 @@ return (1);
 else if (n == 0)
 {
 _putchar('0')
-return 0;
+return (0);
 }
 else
 {
-_putchar('-);
+_putchar('-');
 return (-1);
+}
 }
