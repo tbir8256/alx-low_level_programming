6a28b32997cc3322fd0e8a542627be593cb44a70
tbir8256 <tbir8256@gmail.com>
2022-06-08 04:27:22 -0700

    11-98. Battery Stree

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 = prints natural numbers from n to 98
+ * print_to_98 -> prints natural numbers from n to 98
  * @n: takes in an integer
  */
 void print_to_98(int n)
