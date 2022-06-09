fbf607d7d91e430b996ba71f915ba7280b32d800
tbir8256 <tbir8256@gmail.com>
2022-06-09 13:17:09 -0700

    i.supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper -> check upper case alphas
+ * @c: an integer paramer Return:
+ * something but success
+ */
+
+int _isupper(int c)
+
+{
+
+return (c >= 'A' && c <= 'Z');
+
+}
