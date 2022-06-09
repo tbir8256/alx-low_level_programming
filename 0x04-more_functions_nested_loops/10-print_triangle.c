ffc14ffb2827afbe49cee08f01e5547db6f7b53a
tbir8256 <tbir8256@gmail.com>
2022-06-09 15:00:54 -0700

    10.Triangles/print

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,23 @@
 void print_triangle(int size)
 
 {
+int i;
+int j;
+int k;
 
 if (size <= 0)
 {
 _putchar('\n');
-} 
+}
 else
 {
-int i, j;
 for (i = 1; i <= size; i++)
 {
-for (j = i; j < size; j++)
+for (j = size - 1; j > 0; j--)
 {
 _putchar(' ');
 }
-for (j = 1; j <= i; j++)
+for (k = 0; k <= i; k++)
 {
 _putchar('#');
 }
