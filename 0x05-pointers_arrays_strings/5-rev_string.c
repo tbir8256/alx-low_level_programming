74b426f990c260d5d12fff22df5f1b6727037891
tbir8256 <tbir8256@gmail.com>
2022-06-13 19:45:48 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing a string
+ * @s: parameter s
+ */
+
+void rev_string(char *s)
+
+{
+char tmp;
+int i, l1, l2;
+l1 = 0;
+l2 = 0;
+while (s[l1] != '\0')
+l1++;
+l2 = l1 - 1;
+for (i = 0; i < l1 / 2; i++)
+{
+tmp = s[i];
+s[i] = s[l2];
+s[l2] = tmp;
+l2 -= 1;
+}
+}
