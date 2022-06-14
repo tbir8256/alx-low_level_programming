bd1c32849fba70d95eca897e685f97f90e6c0deb
tbir8256 <tbir8256@gmail.com>
2022-06-13 19:31:33 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print string reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+int i, n;
+n = 0;
+while (s[0] != '\0')
+n++;
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar("\n")
+}
