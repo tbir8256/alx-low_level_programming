02c180d75d5ba866bf50cceaeb144a9367f99069
tbir8256 <tbir8256@gmail.com>
2022-06-16 16:22:04 -0700

    Fix -01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strncat - appedn some chars
+ * _strncat - appednd some chars
  * @dest number 1
  * @src: number 2
  * @n: number 3
@@ -9,19 +9,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
-int i = 0, j = 0;
-
-while (*(dest + i))
-{
-i++;
-}
-while (*(src + j) != '\0' && j < n)
-{
-
-*(dest + i) = src[j];
-i++;
-j++;
-}
+strncat(dest, src, n);
 return (dest);
 }
