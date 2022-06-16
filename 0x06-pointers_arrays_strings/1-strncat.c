aea2235a769ce7f4d086e908fd882f1542ae136c
tbir8256 <tbir8256@gmail.com>
2022-06-16 13:41:26 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy
+ * @dest: number 1
+ * @src: number 2
+ * @n: number 3
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest);
+}
