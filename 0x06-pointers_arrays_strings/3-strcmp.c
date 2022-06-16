49d85d122548af57a45dda301b1711fb369cfda6
tbir8256 <tbir8256@gmail.com>
2022-06-16 14:12:16 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * _strcmp -> for comparing purpose
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+int result;
+result = strcmp(s1, s2);
+return (result);
+}
