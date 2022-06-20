48cd1ca68f05c6bce51883d9bf7110d6943cd282
tbir8256 <tbir8256@gmail.com>
2022-06-20 16:45:05 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - memory set function
+ * @s: string
+ * @n: integer
+ * @b: character
+ * Return: integer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+for (a = 0; a < n; a++)
+	s[a] = b;
+return (s);
+}
