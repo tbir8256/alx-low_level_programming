fe577c4fc231cb674b5e6d6e8416baf53a59b5e7
tbir8256 <tbir8256@gmail.com>
2022-06-22 16:49:57 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints reversed string in recursion
+ * @s: string
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
