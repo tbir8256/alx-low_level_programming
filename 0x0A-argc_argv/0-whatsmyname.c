c6d4c9a09a7b6d150a1176b69aa30576784ba218
tbir8256 <tbir8256@gmail.com>
2022-06-24 19:14:30 -0700

    Fix-

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-printf("%d\n", argc - 1);
+printf("%s\n", argv[0]);
 return (0);
 }
