d232cd42922bd6bb9ea4ada980ad625e0109151f
tbir8256 <tbir8256@gmail.com>
2022-06-24 19:55:10 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
 /**
- * main - a function to print its name
+ * main - this is a function to print its name
  * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: returns sucess
+ * @argv: an array of a command listes
+ * Return: returns 0
  */
 int main(int argc, char *argv[])
 {
@@ -20,3 +21,4 @@ num2 = atoi(argv[2]);
 result = num1 * num2;
 printf("%d\n", result);
 return (0);
+}
