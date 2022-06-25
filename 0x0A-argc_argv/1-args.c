c770384e18328684cc298fd3ff904e183294a6ce
tbir8256 <tbir8256@gmail.com>
2022-06-24 18:41:14 -0700

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,10 @@
  * main - pritnts its name
  * @argc: argc parameter
  * @argv: an array of a command listed
- * Return: 0 for success
+ * Return: returns 0
  */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d \n", argc - 1);
+printf("%d\n", argc - 1);
 return (0);
 }
