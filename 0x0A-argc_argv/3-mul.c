f274413db6cd222135aaaa6c8754f29668102d3d
tbir8256 <tbir8256@gmail.com>
2022-06-24 19:39:38 -0700

    Fix 03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - a function to print its name
@@ -9,15 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-int result, n1, n2;
+int result, num1, num2;
 if (argc != 3)
 {
 printf("%s\n", "Error");
 return (1);
 }
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-result = n1 * n2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
-}
