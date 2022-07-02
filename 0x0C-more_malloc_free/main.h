daaccab639009e20e3432a0513be4d114828379a
tbir8256 <tbir8256@gmail.com>
2022-07-01 21:31:28 -0700

    fix/101

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,10 +11,51 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 
-void errors(void);
+/**
+ * error_exit - prints error with _putchar
+ * and exits with 98
+ * Return: returns 98 with & exit(98)
+ */
+int error_exit(void)
+{
+char *err;
+int i;
+err = "Error";
+for (i = 0; err[i] != '\0'; i++)
+_putchar(err[i]);
+_putchar('\n');
+}
 
-int _strlen(char *s);
+/**
+ * 101-mul.c
+ * check_number - checks if string
+ * has only numbers
+ * @str: string to check
+ * Return: returns 0 f true and
+ * 1 if false
+ */
+int check_number(char *str)
+{
+while (*str != '\0')
+{
+if (*str < '0' || *str > '9')
+return (1);
+str++;
+}
+return (0);
+}
 
-int is_digit(char *s);
+/**
+ * _length - get the length of strings
+ * @str: string to get length
+ * Return: length of string
+ */
+int _length(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+i++;
+return (i);
+}
 
 #endif
