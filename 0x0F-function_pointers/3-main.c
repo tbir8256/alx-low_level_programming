34ded9c4ac30cb2b48f0fb0595de0540e23124e1
tbir8256 <tbir8256@gmail.com>
2022-07-06 18:42:35 -0700

    Fix_03

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
 int arg1, arg2, result;
 char o;
 int (*func)(int, int);
-if (*argc != 4)
+if (argc != 4)
 {
 printf("Error \n");
 exit(98);
 }
 arg1 = atoi(argv[1]);
 arg2 = atoi(argv[3]);
-fun = get_op_func(argv[2]);
+func = get_op_func(argv[2]);
 if (!func)
 {
 printf("Error\n");
@@ -29,7 +29,7 @@ if ((o == '/' || o == '%') && arg2 == 0)
 printf("Error\n");
 exit(100);
 }
-result = fun(arg1, arg2);
-printf(" % \n", result);
+result = func(arg1, arg2);
+printf("%d \n", result);
 return (0);
 }
