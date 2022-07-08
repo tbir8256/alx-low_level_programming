0f472e9c4f1cbf7197d2a31edc99761bc0306ba1
tbir8256 <tbir8256@gmail.com>
2022-07-07 17:43:02 -0700

    Fix-03

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -39,6 +39,11 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -50,5 +55,10 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
