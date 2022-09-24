32c8b9df72bd0897ea4e07a68b5c9fb4a4223191
tbir8256 <tbir8256@gmail.com>
2022-09-23 19:00:50 -0700

    0-hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table;
+hash_table_t *ht;
 unsigned long int i;
-hash_table = malloc(sizeof(hash_table_t));
-if (!hash_table)
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
 return (NULL);
-hash_table->size = size;
-hash_table->array = malloc(sizeof(hash_node_t *) * size);
-if (!hash_table->array)
-{
-free(hash_table);
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
 return (NULL);
-}
 for (i = 0; i < size; i++)
-hash_table->array[i] = NULL;
-return (hash_table);
+ht->array[i] = NULL;
+return (ht);
 }
