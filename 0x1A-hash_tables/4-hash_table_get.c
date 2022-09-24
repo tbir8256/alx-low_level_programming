6c3679bd60460536fe3cd74ac38fde0f8b6b96fb
tbir8256 <tbir8256@gmail.com>
2022-09-23 19:49:03 -0700

    4-hash_table_get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,15 +10,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 hash_node_t *node;
 unsigned long int index;
-if (ht == NULL)
+if (ht == NULL || key == NULL || *key == '\0')
 return (NULL);
-index = key_index((const unsigned char *) key, ht->size);
+index = key_index((const unsigned char *)key, ht->size);
+if (index >= ht->size)
+return (NULL);
 node = ht->array[index];
-while (node != NULL)
-{
-if (strcmp(node->key, key) == 0)
-return (node->value);
+while (node && strcmp(node->key, key) != 0)
 node = node->next;
-}
-return (NULL);
+return ((node == NULL) ? NULL : node->value);
 }
