9fabecc3a68f42907b3019dbdacefa2750aa5200
tbir8256 <tbir8256@gmail.com>
2022-09-27 07:52:22 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,23 +5,19 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size: size of the table
- * Return: hash table on success, NULL on failure
+ * of size (size) and an array
+ * @size: size of tyhe array/ table
+ * Return: table on success or NuLL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht;
-if (!size)
-return (NULL);
-ht = calloc(1, sizeof(hash_table_t));
-if (!ht)
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
 return (NULL);
 ht->size = size;
-ht->array = calloc(size, sizeof(hash_node_t *));
-if (!ht->array)
-{
-free(ht);
+ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+if (ht->array == NULL)
 return (NULL);
-}
 return (ht);
 }
